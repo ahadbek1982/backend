7b0822e8e9cffb52dd074e7dc62823bec86193e0
ahadbek1982 <aqpowerbi@gmail.com>
2023-11-05 14:59:38 +0500

    setting cors

--- backend/settings.py
+++ backend/settings.py
@@ -76,9 +76,9 @@ CORS_ALLOWED_ORIGINS = [
 
     "http://localhost:3000",
     "http://127.0.0.1:9000",
-    "https://frontend-jade-gamma.vercel.app"
-]
 
+]
+CORS_ORIGIN_WHITELIST = ( 'https://backend-jet-nine.vercel.app/', )
 
 CORS_ALLOW_METHODS = (
     "DELETE",
