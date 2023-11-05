89645b258ae8e39e2dcc049b6e19217c238962f5
ahadbek1982 <aqpowerbi@gmail.com>
2023-11-05 11:24:45 +0500

    ch4

--- backend/urls.py
+++ backend/urls.py
@@ -64,5 +64,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
